8edb77fa378a8468bf12dcadebbcd4b85432c62b
100dlswjd <100dlswjd@naver.com>
2023-06-08 22:18:38 +0900

    테스트 코드

--- src/test.py
+++ src/test.py
@@ -1,55 +1,76 @@
 import random
-import time
-
-char = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP\{\}|ASDFGHJKL:\"ZXCVBNM<>?"
-# 길이는 96
-
-def ResetPassword(size : int):
-    password = ""
-    for idx in range(size):
-        password += char[random.randint(0,len(char)-1)]
-    print(password)
-    return password
-
-def CreatePassword(size : int, timer : int):
-    start_time = time.time()
-    while time.time() - start_time < timer:
-        password = ResetPassword(size)
-        time.sleep(random.uniform(0,.1))
-        if time.time() - start_time > timer:
-            print("랜덤 패스워드 생성 : ",password)
-            return password
-        
-def Comparison_password(password : str):
-    randompassowrd = ResetPassword(len(password))
-    while randompassowrd != password:
-        randompassowrd = ResetPassword(len(password))
-        if randompassowrd != password:
-            # print("같지 않음",randompassowrd)
-            pass
-        else:
-            print("찾음 !!")
-            print("찾는 패스워드 : ", password)
-            print("찾은 패스워드 : ", randompassowrd)
-
-
 import string
+import time
+import itertools
+
+class Password:
+    def __init__(self):        
+        self.password = ""
+        self.ord_list = []
+        
+    def create(self, size : int, chars : str = string.ascii_letters + string.digits + string.punctuation):
+        self.ord_list = []
+        self.password = [random.choice(chars) for _ in range(size)]
+        self.ord_list_update()
+        self.password = ''.join(self.password)
+
+    def ord_list_update(self):
+        for idx in self.password:
+            self.ord_list.append(ord(idx))
+
+    def select_create(self, select : list):
+        # 33 ~ 64
+        # 65 ~ 96
+        # 97 ~ 126
+        self.ord_list = []
+        self.password = []
+
+        for idx in select:
+            self.password.append(chr(idx))
+
+        self.password = ''.join(self.password)
+        self.ord_list_update()
+    
+    def show(self):
+        print(f"ord_list : {self.ord_list}")
+        print(f"password : {self.password}")
+        
+    def __del__(self):
+        pass
+
+# print("ascii_letters")
+# for idx in string.ascii_letters:
+#     print(f"{idx} : {ord(idx)}")
+
+# print("digits")
+# for idx in string.digits:
+#     print(f"{idx} : {ord(idx)}")
+
+# print("punctuation")
+# for idx in string.punctuation:
+#     print(f"{idx} : {ord(idx)}")
+
+# string.ascii_letters + string.digits + string.punctuation 33 ~ 126
 
 
-def generate_password(size : int, chars : str = string.ascii_letters + string.digits + string.punctuation) -> str:
-    password = [random.choice(chars) for _ in range(size)]
-    password = ''.join(password)
-    return password
+# print("")
+temp = Password()
+# temp.select_create([65,66])
+# temp.show()
 
+size = 4
 
-def ss():
-    for _ in range(3):
-        print(generate_password(3))
+for tem in itertools.product(range(33,127), repeat=size):
+    temp.select_create(tem)
+    print(temp.show())
 
-    print(generate_password(3, "또는사용할문자열"))
+# for x in range(33,127):
+#     for x_2 in range(33, 127):
+#         for x_3 in range(33, 127):
+#             for x_4 in range(33, 127):
+#                 temp[0] = x
+#                 temp[1] = x_2
+#                 temp[2] = x_3
+#                 temp[3] = x_4
+#                 print(temp)
 
-print(string.ascii_letters)
-print(string.digits)
-print(string.punctuation)
-
-print(string.ascii_letters+string.digits+string.punctuation)
